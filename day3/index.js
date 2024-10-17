f15bd4dcdd65ff5b22a15efeb8f547e8cc97b965
TrishBhardwaj <trishbhardwaj856@gmail.com>
2024-10-17 13:36:53 +0530

    first commit

---
+++ day3/index.js
@@ -0,0 +1,11 @@
+
+/*
+// Function to show an alert
+function showAlert() {
+    alert('Hello! This is a window alert.');
+}
+
+// Adding event listener to the button
+document.getElementById('alertButton').addEventListener('click', showAlert);
+
+*/
